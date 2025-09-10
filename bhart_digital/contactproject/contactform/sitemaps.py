29e07b959c3f03b924343b4cb7b01c758ec51015
KartikEngitech <ekta@kartikengitech.com>
2025-09-10 12:12:35 +0530

    sitemap

--- bhart_digital/contactproject/contactform/sitemaps.py
+++ bhart_digital/contactproject/contactform/sitemaps.py
@@ -27,19 +27,19 @@ class StaticViewSitemap(Sitemap):
         # Return all static view names (the same names you used in urls.py)
         return [
             "home",
-            "about_view",
-            "digital_marketing_view",
-            "game_development_view",
-            "mobile_development_view",
-            "service_view",
-            "web_development_view",
-            "contacts_view",
-            "blog_view",
-            "web_security_view",
-            "business_view",
-            "ai_ml_view",
-            "ui_ux_view",
-            "chatbot_view",
+            "about",
+            "digital_marketing",
+            "game_development",
+            "mobile_development",
+            "services",
+            "web_development",
+            "contact",
+            "blog",
+            "web_security",
+            "business_consultant",
+            "ai_ml",
+            "ui_ux",
+            "chatbot_development",
         ]
 
     def location(self, item):
